e1a67aedb773d6926f0afff9545023647c5dc37a
SEUNGHAN BAEK <84169773+seunghan-baek@users.noreply.github.com>
2021-10-02 02:27:57 +0900

    21.10.01 배열 문제
    bonus문제 풀이 완료 ( 총 풀이 시간 : 6Hrs)

--- src/array2d/test01.java
+++ src/array2d/test01.java
@@ -3,44 +3,38 @@ package array2d;
 public class test01 {
 	public static void main(String[] args) {
 		int[][] a = { { 10, 11, 12, 13, 14 }, { 15, 16, 17, 18, 19 }, { 20, 21, 22, 23, 24 }, { 25, 26, 27, 28, 29 } };
+
 		int rowMin = 0;
 		int rowMax = 3;
 		int colMin = 0;
 		int colMax = 4;
 		int temp = 0;
 		int temp2 = 0;
-//		for (int i = 0; i < 4; i++) { // 입력부
-//			for (int j = 1; j < 5; j++) {
-//				if (i >= rowMin && j <= colMax) {
-//					temp = a[rowMin][j];
-//					a[rowMin][j] = a[rowMin][j - 1];
-//					a[rowMin][j - 1] = temp;
-//				}
-//			}
-//		}
-//		temp = 0;
-//		for (int i = 0; i < 4; i++) { // 입력부
-//			for (int j = 1; j < 5; j++) {
-//				if (i >= rowMax && j == colMax) {
-//					temp = a[i][colMax];
-//					a[i][colMax] = a[i - 1][colMax];
-//					a[i - 1][colMax] = temp;
-//				}
-//			}
-//		}
-		for (int i = 0; i < 4; i++) {
-			for (int j = 1; j < 5; j++) {
-				if (i == j || j > i) {
-					temp = a[i][j - 1];
-					a[i][j - 1] = a[i][j];
-					a[i][j] = temp;
-				}
-				if (i > j) {
-					temp2 = a[i][j];
-					a[i][j] = a[i - 1][j];
-					a[i - 1][j] = temp2;
-				}
+		for (int i = 0; i < 2; i++) {
+			for (int j = colMax - 1; j >= colMin; j--) {
+				temp = a[rowMin][j + 1];
+				a[rowMin][j + 1] = a[rowMin][j];
+				a[rowMin][j] = temp;
 			}
+			for (int k = rowMin; k <= rowMax - 1; k++) {
+				temp = a[k][colMin];
+				a[k][colMin] = a[k + 1][colMin];
+				a[k + 1][colMin] = temp;
+			}
+			for (int j = colMin; j <= colMax - 1; j++) {
+				temp = a[rowMax][j];
+				a[rowMax][j] = a[rowMax][j + 1];
+				a[rowMax][j + 1] = temp;
+			}
+			for (int k = rowMax - 1; k >= rowMin + 1; k--) {
+				temp = a[k + 1][colMax];
+				a[k + 1][colMax] = a[k][colMax];
+				a[k][colMax] = temp;
+			}
+			rowMin++;
+			colMin++;
+			rowMax--;
+			colMax--;
 		}
 // 출력부
 		for (int i = 0; i < 4; i++) {
