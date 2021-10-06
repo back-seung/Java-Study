4fe97b394e1623981d3d9f9b47999d0bae6e0952
SEUNGHAN BAEK <84169773+seunghan-baek@users.noreply.github.com>
2021-10-07 00:34:16 +0900

    단어게임
    ✅부분 점수를 채점 기능
    ✅사용자 지정 단어 설정 기능
    ✅중복 제거 기능

--- src/method/WordGame.java
+++ src/method/WordGame.java
@@ -5,33 +5,74 @@ import java.util.Scanner;
 
 public class WordGame {
 
+	// ## 단어게임 정리
+	// # 1. 하
+	// 1-1. 랜덤하게 단어가 출력되게 한다 													✅
+	// 1-2. 랜덤한 단어를 키보드로 입력하고 채점을 한다. 										✅
+	// # 2. 중
+	// 2-1. 부분 점수를 채점한다.(ex : 제시어가 apple인 경우 app으로 input 했다면 부분점수 적용) 	✅
+	// # 3. 상
+	// 3-1. 단어관리 ( 단어 입력 가능) 													✅
+	// 3-2. 단어별 점수 따로 관리( apple은 10점, banana는 15점 등 단어별로 점수를 다르게 게산) 	✅
+	// 3-3. 중복된 단어는 입력이 안되게 한다. 												✅
+	// # 4. 최상
+	// 4-1. 게임 시작시 사용자명을 입력하고 랭킹을 관리한다. 									❌
+
+	// static String[] randomWord = { "apple", "banana", "cherry", "orange",
+	// "peach", "watermelon", "strawberry", "pear" }; 3-1 구현 후 사용 안함.
+	
+	/* 현재 까지의 bug ? 
+	 *
+
+	 * 1. 
+	 * 
+	 * 2.
+	 * 
+	 * 3.
+	 * 
+	 * 4.
+	 * 
+	 * 5.
+	 * 
+	 * */
 	static Scanner sc = new Scanner(System.in);
 	static Random r = new Random();
 
-	static String[] randomWord = { "apple", "banana", "cherry", "orange", "peach", "watermelon", "strawberry", "pear" };
+	static String[] randomMode = new String[10];
+	static String[] user = new String[5]; // 사용자 저장
 	static String inputWord = ""; // 입력할 단어
 	static String outputWord = ""; // 맞춰야 할 단어
-	static int standardLength = 5; // 단어 기준 길이
-	static int defaultPoint = 5; // 기본 + 포인트
-	static int score = 0; // 총 점수
-	static int playTime = 999; // 게임을 진행할 횟수
 
-	public static void input() { // 단어 입력 기능
+	static int[] userScore = new int[5]; // 사용자 score 저장
+	static int[] Ranking = { 1, 2, 3, 4, 5 };
+	static int playTime = randomMode.length; // 게임을 진행할 횟수
+	static int defaultPoint = 5; // 기본 + 포인트
+	static int standardLength = 5; // 단어 기준 길이
+	static int score = 0; // 총 점수
+	static int over = 0; // randomMode의 길이와 같아졌을 때는 게임 over
+
+	// #1. 단어 입력 기능
+	public static void input() {
 		System.out.println("PRESS ENTER : START \nPRESS \" X \" : END");
 		sc.nextLine();
-		for (;;) {
+
+		// createUser(); // 아직 구현 못함
+
+		createWord(); // 사용자 지정 단어 설정 기능
+		start: for (;;) { // 사용자가 모든 값을 맞췄을 때 (over가 10점일 때) 게임을 종료하기 위해 start 선언
 			for (int i = 0; i < playTime; i++) {
-				outputWord = randomWord[r.nextInt(randomWord.length)]; // randomWord 배열에서 랜덤으로 값을 뽑아옴
+				// 1-1. 랜덤하게 단어가 출력되게 한다.
+				outputWord = randomMode[r.nextInt(randomMode.length)];
 				if (outputWord != "") {
 					System.out.println("랜덤 단어 출력 : " + outputWord);
 					inputWord = sc.nextLine(); // 입력값
-					if (inputWord.equals(outputWord)) { // 입력값과 같은가
-						System.out.println("추가 점수 : " + (inputWord.length() - standardLength));
+					// 1-2. 랜덤한 단어를 키보드로 입력하고 채점을 한다.
+					if (inputWord.equals(outputWord)) {
 						checkLength(inputWord);
 						delWord(outputWord);
 						System.out.println("현재 스코어 : " + score);
 						break;
-					} else if (outputWord.length() < inputWord.length()) {
+					} else if (outputWord.length() < inputWord.length()) { // 입력된 값이 제시어보다 길 때 0점 처리
 						score += 0;
 						System.out.println("현재 스코어 : " + score);
 					} else {
@@ -42,19 +83,19 @@ public class WordGame {
 					continue;
 				}
 			}
+			if (over == 10) {
+				for (int i = 0; i < userScore.length; i++) {
+					userScore[i] += score; // createUser의 기능의 일부분
+					System.out.println("게임이 끝났습니다. 감사합니다.\n최종 스코어 : " + userScore[i]);
+					break;
+				}
+				break start;
+			}
 		}
 	}
 
-	public static int checkLength(String inputWord) { // 추가된 길이만큼 추가 점수 적용
-		if (inputWord.length() > standardLength) {
-			score += inputWord.length();
-		} else {
-			score += defaultPoint;
-		}
-		return score;
-	}
-
-	public static int correctCharCount(String inputWord) { // 입력값과 같지 않을 때 부분 점수 적용
+	// 2-1. 부분 점수를 채점한다.(ex : 제시어가 apple인 경우 app으로 input 했다면 부분점수 적용)
+	public static int correctCharCount(String inputWord) {
 		int cnt = 0;
 		for (int i = 0; i < inputWord.length(); i++) {
 			if (inputWord.charAt(i) == outputWord.charAt(i)) { // input과 output의 index가 서로 같을 때 값 또한 같은가?
@@ -69,18 +110,46 @@ public class WordGame {
 		return score;
 	}
 
+	// #3- 1. 사용자가 지정하여 단어를 선언
+	public static void createWord() {
+		System.out.println("사용자 지정 단어를 입력해주세요");
+		for (int i = 0; i < randomMode.length; i++) { // 입력부
+			System.out.println("# 1번 배열의 " + i + "번 째 이름 입력 : ");
+			String word = sc.nextLine();
+			randomMode[i] = word;
+		}
+	}
+
+	// 3-2. 단어별 점수 따로 관리( apple은 10점, banana는 15점 등 단어별로 점수를 다르게 게산)
+	public static int checkLength(String inputWord) {
+		if (inputWord.length() > standardLength) {
+			score += inputWord.length();
+		} else {
+			score += defaultPoint;
+		}
+		return score;
+	}
+
+	// 3-3. 중복된 단어는 입력이 안되게 한다.
 	public static void delWord(String inputWord) {
-		for (int i = 0; i < randomWord.length; i++) {
-			if (randomWord[i].equals(inputWord)) {
-				randomWord[i] = "";
+		for (int i = 0; i < randomMode.length; i++) {
+			if (randomMode[i].equals(inputWord)) {
+				randomMode[i] = "";
+				over++; // 정답일 때 over++
 			}
 		}
 	}
+	// 4-1. 게임 시작시 사용자명을 입력하고 랭킹을 관리한다.
+//	public static void createUser() { 
+//		System.out.println("사용자 ID를 입력해주세요");
+//		String userID = sc.nextLine();
+//		for (int i = 0; i < user.length; i++) {
+//			user[i] = userID;
+//			break;
+//		}
+//	}
 
 	public static void main(String[] args) {
 		input();
 	}
-
 }
-// 단어게임 정리 =>
-// 
